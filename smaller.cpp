8d3cf74ef084db0a7112af341a46e50a2f0105e8
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-25 12:28:28 -0400

    Create smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Enter the integer: " << end1;
+  int number1 = 0;
+  cin >> number1;
+  
+  cout << "Enter the second integer: " << end1;
+  int number2 = 0;
+  cin >> number1;
+  
+  if (number1 > number2){
+    
+  }
+    
+  if (number2 > number1){
+    
+  }
+    
+  cout << << " is the smaller integer." << end1;
+}
