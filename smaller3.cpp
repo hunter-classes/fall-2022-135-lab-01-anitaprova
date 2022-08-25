93a8a50855356ef4b97f5bbc520b5ccef9cf6462
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-25 13:13:58 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int small = 0;
+  cout << "Enter the integer: " << endl;
+	int number1 = 0;
+	cin >> number1;
+	
+	cout << "Enter the second integer: " << endl;
+	int number2 = 0;
+	cin >> number2;
+  
+  cout << "Enter the third integer: " << endl;
+  int number3 = 0;
+  cin >> number3;
+	
+	if (number1 > number2){
+		small = number2;
+	}
+  
+  if (number3 < small){
+    small = number3;
+	}
+  return 0;
+}
