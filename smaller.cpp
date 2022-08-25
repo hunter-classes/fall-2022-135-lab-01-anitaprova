50d11d6b82d4580a1dd3e3c35d951c2b6d5fe638
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-25 12:59:14 -0400

    Update smaller.cpp

--- smaller.cpp
+++ smaller.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main()
 {
-  cout << "Enter the integer: " << end1;
-  int number1 = 0;
-  cin >> number1;
-  
-  cout << "Enter the second integer: " << end1;
-  int number2 = 0;
-  cin >> number1;
-  
-  if (number1 > number2){
-    cout << number2 << " is the smaller integer." << end1;
-  }
+	cout << "Enter the integer: " << endl;
+	int number1 = 0;
+	cin >> number1;
+	
+	cout << "Enter the second integer: " << endl;
+	int number2 = 0;
+	cin >> number1;
+
+	if (number1 > number2){
+		cout << number2 << " is the smaller integer." << endl;
+	}
     
-  if (number2 > number1){
-    cout << number1 << " is the smaller integer." << end1; 
-  }
+	if (number2 > number1){
+		cout << number1 << " is the smaller integer." << endl; 
+	}
 }
