ede500eba2a47ef779d23a6fac5e2ae3e3e68b87
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-27 15:54:53 -0400

    Update month.cpp

--- month.cpp
+++ month.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	cout << "Enter the year: " << endl;
+	int year = 0;
+	cin >> year;
+	
+	if (year % 4 != 0) 
+	{
+		cout << "Common Year" << endl;
+	}
+	else if (year % 100 != 0)
+	{
+		cout << "Leap Year" << endl;
+	}
+	else if(year % 400 != 0)
+	{
+		cout << "Common Year" << endl;
+	}
+	
+	return 0;
+}
