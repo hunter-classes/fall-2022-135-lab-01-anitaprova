443db56fe26df3f43b33d0f28181c7517904f9a0
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-27 16:10:06 -0400

    Update and rename m.cpp to month.cpp

---
+++ month.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter year: " << endl;
+	int year = 0;
+	cin >> year;
+	
+	cout << "Enter month: " << endl;
+	int month = 0;
+	cin >> month;
+	
+	if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+	{
+		cout << "31 Days" << endl;
+	}
+	
+	if ()
+	{
+		
+	}
+	
+	return 0;
+}
