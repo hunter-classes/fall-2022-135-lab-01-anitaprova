563d9a71ee3f1eed372fd50f02a9ffc02d6d0381
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-01 11:46:22 -0400

    fixed format

--- leap.cpp
+++ leap.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-	cout << "Enter the year: " << endl;
+	cout << "Enter the year: ";
 	int year = 0;
 	cin >> year;
 	
 	if (year % 4 != 0) 
 	{
-		cout << "Common Year" << endl;
+		cout << "\nCommon Year" << endl;
 	}
 	else if (year % 100 != 0)
 	{
-		cout << "Leap Year" << endl;
+		cout << "\nLeap Year" << endl;
 	}
 	else if(year % 400 != 0)
 	{
-		cout << "Common Year" << endl;
+		cout << "\nCommon Year" << endl;
 	}
 	else
 	{
-		cout << "Leap Year" << endl;
+		cout << "\nLeap Year" << endl;
 	}
 	
 	return 0;
