e3cdc5187c0985da752c9e783f5e7bb9c595d2a7
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-01 11:40:46 -0400

    changed phrasing

--- smaller.cpp
+++ smaller.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main()
 {
-	cout << "Enter the integer: ";
+	cout << "Enter the first number: ";
 	int number1 = 0;
 	cin >> number1;
 	
-	cout << "Enter the second integer: ";
+	cout << "Enter the second number: ";
 	int number2 = 0;
 	cin >> number2;
 	
 	if (number1 > number2){
-		cout << number2 << " is the smaller integer." << endl;
+		cout << "The smaller of the two is " << number2 << endl;
 	}
     
 	if (number2 > number1){
-		cout << number1 << " is the smaller integer." << endl; 
+		cout << "The smaller of the two is " << number1 << endl; 
 	}
 }
